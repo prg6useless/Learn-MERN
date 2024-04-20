794f478a60503c09b9115a6ffcf1f7819433439c
Saral Sainju <sainju.saral433@gmail.com>
2024-04-20 13:59:44 +0545

    new file

---
+++ node/app.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("Hello World!");
+  })
+  .listen(8080);
+console.log("App running on port 8080");
