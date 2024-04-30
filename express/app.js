90dd80426eaabc07b77850fefac6b512e423cf23
Saral Sainju <sainju.saral433@gmail.com>
2024-04-30 17:27:35 +0545

    express boilerplate

---
+++ express/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = Number(process.env.PORT);
+
+app.get("/", (request, response) => {
+  response.json({ message: "Hello World" });
+});
+
+app.listen(port, () => {
+  console.log(`Appilcation is running at port ${port}`);
+});
