db1565a85df6e2096ffed2d633dad4e51cdaf2c1
Saral Sainju <sainju.saral433@gmail.com>
2024-05-07 15:51:57 +0545

    check role module

--- express/app.js
+++ express/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const route = require("./routes/index");
+const role = require("./utils/secure");
 
 const app = express();
 const port = Number(process.env.PORT);
@@ -8,6 +9,8 @@ const port = Number(process.env.PORT);
 // allows tha parsing of request body as json
 app.use(express.json());
 app.use("/", route);
+
+const myRole = role("user");
 
 // comes here from next() during error
 // Error Handling
@@ -17,7 +20,7 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(port, () => {
-  console.log(`Appilcation is running at port ${port}`);
+  console.log(`Appilcation is running at port ${port} and ${myRole}`);
 });
 
 // research about default destructure; destructuring assignment
--- express/utils/secure.js
+++ express/utils/secure.js
@@ -1,1 +1,5 @@
 // check role module
+
+const checkRole = (role) => (role === "admin" ? true : false);
+
+module.exports = checkRole;
