75b24253a654e9f6a3443a5a72b194dbf5961da8
Saral Sainju <sainju.saral433@gmail.com>
2024-05-31 17:43:11 +0545

    functions review

--- frontend/html/index.js
+++ frontend/html/index.js
@@ -13,3 +13,60 @@ const formSubmit = (e) => {
     document.getElementById("systemMsg").innerHTML = "";
   }, 3000);
 };
+
+const num = "2345678910";
+
+const displayNumber = (number) =>
+  number.length === 10 ? "********".concat(number.slice(8)) : "Inavlid number";
+
+console.log(displayNumber(num));
+
+const val = "37xT036vB4772427Fs841590878Qm283"; //32 char string
+
+const displayValue = (value) =>
+  value
+    .slice(0, 10)
+    .concat("...")
+    .concat(value.slice(value.length - 10, value.length));
+
+console.log(displayValue(val));
+
+// assignment
+
+// copy url to clipboard feature
+
+const vowelCount = (string) => {
+  let count = 0;
+  let vowelObject = {};
+  let vowels = "aeiou";
+  for (let char of string) {
+    if (vowels.includes(char)) {
+      if (!vowelObject[char]) vowelObject[char] = 1;
+      else {
+        vowelObject[char] += 1;
+      }
+    }
+  }
+  return vowelObject;
+};
+
+console.log(vowelCount("koutaro"));
+
+// convert full name to proper case
+
+const toProperCase = (string) =>
+  string
+    .split(" ")
+    .map((items) => items.charAt(0).toUpperCase().concat(items.slice(1)))
+    .join(" ");
+
+console.log(toProperCase("yamashita mizuki"));
+
+// check nepali number
+
+const checkNumber = (string) =>
+  string.length === 10 && string.match(/9[8/7]\d{8}/gi) ? true : false;
+
+console.log(checkNumber("9742342435"));
+
+(() => {})();
