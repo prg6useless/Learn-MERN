26f64a7e2a52bbe924d655fc9e59b3d2542113f7
Saral Sainju <sainju.saral433@gmail.com>
2024-06-03 16:15:58 +0545

    array methods

--- express/utils/text.js
+++ express/utils/text.js
@@ -1,5 +1,8 @@
 const slugify = require("slugify");
 
+// example of pure function
+// expected to give the same output everytime regardless of the input
+
 const slugger = (text) => {
   return slugify(text, {
     replacement: "-", // replace spaces with replacement character, defaults to `-`
--- frontend/html/index.js
+++ frontend/html/index.js
@@ -36,7 +36,6 @@ console.log(displayValue(val));
 // copy url to clipboard feature
 
 const vowelCount = (string) => {
-  let count = 0;
   let vowelObject = {};
   let vowels = "aeiou";
   for (let char of string) {
@@ -69,4 +68,100 @@ const checkNumber = (string) =>
 
 console.log(checkNumber("9742342435"));
 
-(() => {})();
+// (() => {})(); -> IIFE
+
+// fibonacci series
+const fibonacci = (num) => {
+  let num1 = 0;
+  let num2 = 1;
+  let sum;
+
+  for (let i = 1; i <= num; i++) {
+    console.log(num1);
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
+  return num2;
+};
+console.log("Fibonacci(5): " + fibonacci(8));
+// console.log("Fibonacci(8): " + fibonacci(8));
+
+// reverse array
+const array = [1, 2, 3, 4, 5];
+let reverseArray = [];
+for (i = array.length - 1; i >= 0; i--) {
+  reverseArray.push(array[i]);
+}
+console.log(reverseArray);
+console.log(array.reverse());
+
+// sum of array
+const array2 = [1, 2, 3, 4, 5, 6];
+let sum = 0;
+for (let i = 0; i < array2.length; i++) {
+  sum += array2[i];
+}
+console.log(sum);
+const sumArray = array2.reduce((num1, num2) => num1 + num2, 0);
+console.log(sumArray);
+
+// array of only even number
+let even = 0;
+let evenArray2 = [];
+for (let i = 0; i < array2.length; i++) {
+  if (array2[i] % 2 === 0) evenArray2.push(array2[i]);
+}
+const evenArray = array2.filter((items) => items % 2 === 0);
+console.log(evenArray, evenArray2);
+
+// no of items in array
+let array3 = [1, 1, 2, 2, 2, 3, 3, 3, 3, 4];
+
+const countArray = (array, number) => {
+  let count = 0;
+  for (let item of array3) {
+    if (item === number) count++;
+  }
+  return count;
+};
+
+console.log("number of occurence of 2 : ", countArray(array3, 2));
+
+const arrayItems = (array) => {
+  let countObj = {};
+  for (let item of array) {
+    if (!countObj[item]) countObj[item] = 1;
+    else countObj[item] += 1;
+  }
+  return countObj;
+};
+console.log(arrayItems(array3));
+
+// remove duplicate from array
+
+const newArray = array3.filter(
+  (items, index) => array3.indexOf(items) === index
+);
+
+console.log(newArray);
+
+// sort bu author name
+
+const names = [
+  { author: "Victor", title: "The Road Ahead" },
+  { author: "Celine", title: "Harry Potter" },
+  { author: "Max", title: "Carry On" },
+];
+
+names.sort((a, b) => {
+  if (a.author < b.author) {
+    return -1;
+  }
+  if (a.author > b.author) {
+    return 1;
+  }
+  return 0;
+});
+
+console.log(names);
