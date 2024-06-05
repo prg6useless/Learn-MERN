d69dc0c1de2790d6d7fd8a0a972c6c5540302f3b
Saral Sainju <sainju.saral433@gmail.com>
2024-06-05 17:59:33 +0545

    fetch and axios

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -1,35 +1,25 @@
-// fetch -> used for browser
-// const axios = require("axios");
+// use axios
 const products = document.getElementById("products");
+
 const instance = axios.create({
   baseURL: "https://dummyjson.com",
   timeout: 1000,
-  // headers: { "X-Custom-Header": "foobar" },
 });
 
 const getProducts = async (url) => {
   try {
-    const controller = new AbortController();
-    const { data } = await instance.get(url, {
-      signal: controller.signal,
-    });
+    const { data } = await instance.get(url);
     console.log(data);
-    // const data = getProducts("/products");
-
     data.products.map((product) => {
-      const productElement = document.createElement("div");
-      productElement.innerHTML = `
-        <h2>${product.title}</h2>
-        <p>${product.price}</p>
-        <p>${product.description}</p>
-      `;
-      products.appendChild(productElement);
+      const div = document.createElement("div");
+      div.innerHTML = `
+              <h3>${product.title}</h3>
+              <p>${product.price}</p>
+              <p>${product.description}</p>
+          `;
+      products.appendChild(div);
     });
-
-    controller.abort();
   } catch (error) {
     console.log(error);
   }
 };
-
-// getProducts("/products");
