419d592db19a9d39cbf835715ea050488bf98aed
Saral Sainju <sainju.saral433@gmail.com>
2024-06-06 17:46:58 +0545

    axios api two

--- frontend/api/axios.js
+++ frontend/api/axios.js
@@ -23,3 +23,45 @@ const getProducts = async (url) => {
     console.log(error);
   }
 };
+
+const login = async () => {
+  try {
+    const email = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const payload = {
+      username: email,
+      password,
+    };
+    const controller = new AbortController();
+    const { data } = await instance.post("/auth/login", payload, {
+      signal: controller.signal,
+    });
+    localStorage.setItem("userCredentials", JSON.stringify(data));
+    sessionStorage.setItem("userCredentials", JSON.stringify(data));
+    const userInfo = localStorage.getItem("userCredentials");
+    document.getElementById("userInfo").innerHTML = userInfo;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getData = async () => {
+  try {
+    const controller = new AbortController();
+
+    const { token } = JSON.parse(localStorage.getItem("userCredentials"));
+    console.log(token);
+    const { data } = await instance.get(
+      "/auth/me",
+      (headers = {
+        Authorization: `Bearer ${token}`,
+      }),
+      {
+        signal: controller.signal,
+      }
+    );
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
---
+++ frontend/api/questions.md
@@ -0,0 +1,9 @@
+- Local Storage
+
+- Session Storage
+
+- Cookies
+
+- Can session storage be used across multiple windows or tabs?
+
+  No, whoever created session foreg, tab or window. Only the tab or window that has access to that session. If I have share across multuple tab/ window, we have to use Local Storage
