24525be25e342ac2bc097eb080a2d0a1b9a6070f
Saral Sainju <sainju.saral433@gmail.com>
2024-06-11 17:21:23 +0545

    useState Hook

--- vite/src/App.jsx
+++ vite/src/App.jsx
@@ -1,8 +1,12 @@
 // import Goal from "./Components/Goal";
+import { useState } from "react";
 import List from "./Components/List";
 import Login from "./Components/Login";
 
 function App() {
+  const [color, setColor] = useState("red");
+  const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
   const car = [
     {
       name: "BMW",
@@ -13,14 +17,55 @@ function App() {
       model: 2015,
     },
   ];
+
+  const [person, setPerson] = useState({
+    name: "Saral",
+    age: 21,
+    gender: "male",
+  });
   return (
     <>
       {/* <Goal /> */}
       <br />
       <Login />
       <List data={car} />
+      <br />
+      <button onClick={() => setColor("black")}>Change Color</button>
+      <br />
+      {color}
+      <br />
+      <br />
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <h3>{count}</h3>
+      <button onClick={() => setCount(count - 1)}>Subtract</button>
+      <br />
+      <br />
+      <button onClick={() => setLoading(!loading)}>Click Me</button>
+      {loading && <Content />}
+      <br />
+      <br />
+      <input
+        placeholder="enter name"
+        onChange={(e) =>
+          setPerson((prev) => {
+            return { ...prev, name: e.target.value };
+          })
+        }
+      />
+      {/*controlled form,*/}
+      <br />
+      <br />
+      {person?.name}
+      <br />
+      {person?.age}
+      <br />
+      {person?.gender}
     </>
   );
 }
 
 export default App;
+
+export const Content = () => {
+  return <div>Some Content</div>;
+};
--- vite/src/Components/Login.jsx
+++ vite/src/Components/Login.jsx
@@ -2,49 +2,39 @@
 import { useState } from "react";
 
 const Login = () => {
-  const [dataValid, setDataValid] = useState(false);
+  const [dataInvalid, setDataInvalid] = useState(false);
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
   // const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
-    const name = document.getElementById("name1").value;
-    const pw = document.getElementById("password1").value;
-    name != pw ? setDataValid(true) : alert("login");
+    if (name != password) setDataInvalid(true);
+    else {
+      setDataInvalid(false);
+      console.log("login");
+    }
   };
 
   return (
     <>
-      {/* <Card style={{ width: "18rem" }}>
-        <Card.Body>
-          <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We&apos;ll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card> */}
       Login
       <br />
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="name" id="name1"></input>
-        <input type="password" placeholder="password" id="password1"></input>
+        <input
+          type="text"
+          placeholder="name"
+          id="name1"
+          onChange={(e) => setName(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="password"
+          id="password1"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
         <button type="submit">Submit</button>
       </form>
-      {dataValid && <Error />}
+      {dataInvalid && <Error />}
     </>
   );
 };
